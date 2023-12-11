ce4bcde8a49d5a97b91d891625c17af3fc501ad4
Sergio Esteban <Sergioss772022@gmail.com>
2023-12-12 00:38:26 +0100

    initial-commit2

--- router/router.js
+++ router/router.js
@@ -3,12 +3,19 @@ const authController = require("../controllers/auth.controller");
 const usersController = require("../controllers/users.controller");
 const authMiddleware = require("../middlewares/auth.middlewares");
 
+
+const GOOGLE_SCOPES = [
+  'https://www.googleapis.com/auth/userinfo.email',
+  'https://www.googleapis.com/auth/userinfo.profile'
+]
+
 router.get("/", authMiddleware.isAuthenticated, (req, res, next) => {
   res.render("home");
 });
 
 
 
+// auth
 
 router.get("/login", authMiddleware.isNotAuthenticated, authController.login);
 router.post("/login", authMiddleware.isNotAuthenticated, authController.doLogin);
@@ -17,6 +24,11 @@ router.post("/register", authMiddleware.isNotAuthenticated, authController.doReg
 router.get("/logout", authMiddleware.isAuthenticated, authController.logout);
 router.get("/activate/:token", authController.activate);
 
+// Google auth
+router.get('/auth/google', authMiddleware.isNotAuthenticated, passport.authenticate('google-auth', { scope: GOOGLE_SCOPES }));
+router.get('/auth/google/callback', authMiddleware.isNotAuthenticated, authController.doLoginGoogle)
+
+// user
 
 router.get("/profile", authMiddleware.isAuthenticated, usersController.profile);
 
