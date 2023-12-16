0140b09c50b12c9b4e102db7b26641030ec4564d
sergioironhacker <sergioss772022@gmail.com>
2023-12-16 09:58:42 +0100

    errors-solved

--- controllers/users.controller.js
+++ controllers/users.controller.js
@@ -4,36 +4,36 @@ const User = require('../models/User.model');
 
 
 module.exports.profile = (req, res, next) => {
-    const currentUser = req.session.currentUser; 
-   
-    if (currentUser) {
-      res.render('users/profile', { currentUser }); 
-      console.log('currentUser:', currentUser);
-    } else {
-      res.redirect('/login'); 
-    }
-  };
+  const currentUser = req.session.currentUser;
 
- // QR
-  module.exports.qr = (req, res, next) => {
-  
+  if (currentUser) {
+    res.render('users/profile', { currentUser });
+    console.log('currentUser:', currentUser);
+  } else {
+    res.redirect('/login');
+  }
+};
+
+// QR
+module.exports.qr = (req, res, next) => {
+
   const email_address = 'proyectoironhack@gmail.com';
 
   const subject = 'Ayuda';
 
-  
+
   const body = 'Si necesitas ayuda, contáctanos 😉';
 
 
   const mailto_link = `mailto:${email_address}?subject=${encodeURIComponent(subject)}&body=${encodeURIComponent(body)}`;
 
-  
+
   qr.toDataURL(mailto_link, (err, qrCode) => {
     if (err) {
       console.error(err);
       return res.status(500).send('Error al generar el código QR');
     }
-    
+
     res.send(` <html>
     <head>
       <style>
@@ -56,20 +56,20 @@ module.exports.profile = (req, res, next) => {
     </body>
   </html>`);
   });
-  }
-  
+}
 
-  // image 
-  module.exports.profileUpload = async (req, res, next) => {
-try {
-      const currentUserId = req.session.currentUser._id
-      const updateUser = await User.findByIdAndUpdate(currentUserId, {
-        picture: req.file.path 
-      }, {new: true})
-      req.session.currentUser = updateUser
-      res.redirect('/profile')
-    }
-    catch(error) {
-        console.log(error)
-    }
+
+// image 
+module.exports.profileUpload = async (req, res, next) => {
+  try {
+    const currentUserId = req.session.currentUser._id
+    const updateUser = await User.findByIdAndUpdate(currentUserId, {
+      picture: req.file.path
+    }, { new: true })
+    req.session.currentUser = updateUser
+    res.redirect('/profile')
   }
+  catch (error) {
+    console.log(error)
+  }
+}
