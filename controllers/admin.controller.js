6628b6596498bdeefa59eaccfa1bde2eac762d26
Victor Junquera <victorjunquera@eventalista.es>
2023-12-16 11:38:30 +0100

    added admin news CRUD

--- controllers/admin.controller.js
+++ controllers/admin.controller.js
@@ -1,4 +1,5 @@
-const User = require('../models/User.model');
+const User = require('../models/User.model')
+const News = require('../models/News.model')
 
 module.exports.userlist = function(req, res, next) {
   User.find()
@@ -6,4 +7,58 @@ module.exports.userlist = function(req, res, next) {
     .catch(error => next(error));
 }
 
+module.exports.newslist = function(req, res, next) {
+  News.find()
+    .then(news => res.render("admin/newslist", { news }))
+    .catch(error => next(error));
+}
 
+module.exports.delete = (req, res, next) => {
+  const { id } = req.params;
+
+  News.findByIdAndDelete(id)
+    .then(() => {
+      res.redirect('/admin/newslist');
+    })
+    .catch(next)
+}
+
+module.exports.update = (req, res, next) => {
+  const { id } = req.params;
+
+  News.findById(id)
+    .then(news => {
+      res.render('admin/news-form', { news });
+    })
+    .catch(next)
+}
+
+module.exports.doUpdate = (req, res, next) => {
+  const { id } = req.params;
+  if (req.file) {
+    req.body.image = req.file.path;
+  }
+
+  News.findByIdAndUpdate(id, req.body, { new: true })
+    .then(news => {
+      res.redirect(`/news/${news._id}`);
+    })
+    .catch(next)
+}
+
+module.exports.create = (req, res, next) => {
+  res.render('admin/news-form');
+}
+
+
+module.exports.doCreate = (req, res, next) => {
+  if (req.file) {
+    req.body.image = req.file.path;
+  }
+
+  News.create(req.body)
+    .then(news => {
+      res.redirect(`/news/${news._id}`);
+    })
+    .catch(next)
+}
